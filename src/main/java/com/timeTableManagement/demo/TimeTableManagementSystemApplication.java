5ea565647f5d213bc615a4c3a9abcb40ca6ec58d
sabeelmhd <sabeelzaim@gmail.com>
2024-03-07 21:08:05 +0530

    first commit

---
+++ src/main/java/com/timeTableManagement/demo/TimeTableManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.timeTableManagement.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTableManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTableManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timeTableManagement/demo/TimeTableManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timeTableManagement.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTableManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
